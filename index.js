b8151013b55d014e04cb923dc06def5cd2e308c0
OwenOchieng <owen.ochieng@student.moringaschool.com>
2022-06-24 12:00:11 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your Circle class here
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    get diameter() {
+        return this.radius * 2;
+    }
+    get circumference() {
+        return this.radius * 2 * Math.PI;
+    }
+    get area() {
+        return this.radius * this.radius * Math.PI;
+    }
+    set diameter(diameter) {
+        this.radius = diameter / 2;
+    }
+    set circumference(circumference) {
+        this.radius = circumference / Math.PI / 2;
+    }
+    set area(area) {
+        this.radius = Math.sqrt(area / Math.PI);
+    }
+}
